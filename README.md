87d97aa6b88919d3af5a21fb7fa2c1af5bb9f6e4
Your Name <you@example.com>
2021-01-19 10:22:01 +0100

    file for repository uploaded

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rain_alert
 Based on a call to Open Weather API send a rain alert message via Twilio if rain is predicted
+
+Please note that environment variables are used for the Open Weather Map API-Key and the Twilio account data and phone number.
+
+If you set up an account at https://openweathermap.org/ and https://www.twilio.com/, you can set your own values.
+
+
---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+import os
+from twilio.rest import Client
+
+
+
+api_key = os.environ.get('OWM_API_KEY')
+phone_number = os.environ.get("TWILIO_PHONE_NUMBER")
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
+
+parameters = {
+    'lat': 48.13743,
+    'lon': 11.57549,
+    'appid': api_key,
+    'exclude': "current,minutely,daily"
+}
+
+response = requests.get("https://api.openweathermap.org/data/2.5/onecall", params=parameters)
+response.raise_for_status()
+
+weather_data = response.json()
+
+
+weather_ids = [item['weather'][0]['id'] for item in weather_data['hourly'][0:12]]
+
+print(weather_ids)
+
+res = any(item['weather'][0]['id'] < 700 for item in weather_data['hourly'][0:12])
+
+if res:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="Bring an umbrella.",
+        from_=phone_number,
+        to='+4917685583232'
+    )
+
+    print(message.status)
